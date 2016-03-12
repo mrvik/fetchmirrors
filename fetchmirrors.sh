361fcd406d8945d6f48752f88665b4f34e48a2da
deadhead420 <deadhead3492@gmail.com>
2016-03-12 13:46:24 -0500

    Added no color option

--- fetchmirrors.sh
+++ fetchmirrors.sh
@@ -2,6 +2,7 @@
 
 usage() {
 
+	## Display help / usage options menu
 	echo "${Yellow} Usage:${Green} $this <opts>"
 	echo
 	echo "${Yellow} Updates pacman mirrorlist directly from archlinux.org"
@@ -13,6 +14,9 @@ usage() {
 	echo "   -l --list"
 	echo "${Yellow}   Display list of country codes"
 	echo
+	echo "${Green}   -n --nocolor"
+	echo "${Yellow}   Disable color prompts"
+	echo
 	echo "${Green}   -h --help"
 	echo "${Yellow}   Display this help message"
 	echo
@@ -20,7 +24,8 @@ usage() {
 }
 
 list() {
-
+	
+	## Display list of country codes to user in terminal
 	echo "${Yellow}Country codes:${ColorOff}"
 	echo "$countries" | column -t
 	echo
@@ -31,23 +36,30 @@ list() {
 
 get_opts() {
 	
+	## Set this variable and countries variable
 	this=${0##*/}
 	countries=$(echo -e "1) AT Austria - 2) AU  Australia - 3) BE Belgium\n4) BG Bulgaria - 5) BR Brazil - 6) BY Belarus\n7) CA Canada - 8) CL Chile - 9) CN China \n10) CO Columbia - 11) CZ Czech-Republic - 12) DE Germany\n13) DK Denmark - 14) EE Estonia - 15) ES Spain\n16) FI Finland - 17) FR France - 18) GB United-Kingdom\n19) HU Hungary - 20) IE Ireland - 21) IL Isreal\n22) IN India - 23) IT Italy - 24) JP Japan\n25) KR Korea - 26) KZ Kazakhstan - 27) LK Sri-Lanka\n28) LU Luxembourg - 29) LV Lativia - 30) MK Macedonia\n31) NC New-Caledonia - 32) NL Netherlands - 33) NO Norway\n34) NZ New-Zealand - 35) PL Poland - 36) PT Portugal\n37) RO Romania - 38) RS Serbia - 39) RU Russia\n40) SE Sweden - 41) SG Singapore - 42) SK Slovakia\n43) TR Turkey - 44) TW Taiwan - 45) UA Ukraine\n46) US United-States - 47) UZ Uzbekistan - 48) VN Viet-Nam\n49) ZA South-Africa")
 	
-	Green=$'\e[0;32m';
-	Yellow=$'\e[0;33m';
-	Red=$'\e[0;31m';
-	ColorOff=$'\e[0m';
+	## Set colors is user selects nocolor option disable color
+	if (<<<$* grep "\-\-nocolor\|\-n" &> /dev/null); then
+		Green=$'\e[0m';
+		Yellow=$'\e[0m';
+		Red=$'\e[0m';
+		ColorOff=$'\e[0m';
+	else
+		Green=$'\e[0;32m';
+		Yellow=$'\e[0;33m';
+		Red=$'\e[0;31m';
+		ColorOff=$'\e[0m';
+	fi
 
-	case "$1" in
-		-l|--list)
-			list
+	case "$*" in
+		$(<<<"$*" grep "\-l\|\-\-list")) list
 		;;
-		-h|--help)
-			usage
+		$(<<<"$*" grep "\-h\|\-\-help")) usage
 		;;
-		-c|--country)
-			if (<<<$countries grep -o "$2"); then
+		$(<<<"$*" grep "\-c\|\-\-country"))
+			if (<<<$countries grep -o "$2" &> /dev/null); then
 				country_code="$2"
 				get_list
 			else
