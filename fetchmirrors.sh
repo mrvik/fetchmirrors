128375450c67f81ac80b23fdc60cd50240b5641d
deadhead420 <deadhead3492@gmail.com>
2016-03-12 16:58:29 -0500

    added no confirm option

--- fetchmirrors.sh
+++ fetchmirrors.sh
@@ -17,6 +17,9 @@ usage() {
 	echo "${Green}   -n --nocolor"
 	echo "${Yellow}   Disable color prompts"
 	echo
+	echo "${Green}   -q --noconfirm"
+	echo "${Yellow}   Disable confirmation messages (Use this to run $this automatically without confirmation)"
+	echo
 	echo "${Green}   -h --help"
 	echo "${Yellow}   Display this help message"
 	echo
@@ -38,10 +41,11 @@ get_opts() {
 	
 	## Set this variable and countries variable
 	this=${0##*/}
+	confirm=true
 	countries=$(echo -e "1) AT Austria - 2) AU  Australia - 3) BE Belgium\n4) BG Bulgaria - 5) BR Brazil - 6) BY Belarus\n7) CA Canada - 8) CL Chile - 9) CN China \n10) CO Columbia - 11) CZ Czech-Republic - 12) DE Germany\n13) DK Denmark - 14) EE Estonia - 15) ES Spain\n16) FI Finland - 17) FR France - 18) GB United-Kingdom\n19) HU Hungary - 20) IE Ireland - 21) IL Isreal\n22) IN India - 23) IT Italy - 24) JP Japan\n25) KR Korea - 26) KZ Kazakhstan - 27) LK Sri-Lanka\n28) LU Luxembourg - 29) LV Lativia - 30) MK Macedonia\n31) NC New-Caledonia - 32) NL Netherlands - 33) NO Norway\n34) NZ New-Zealand - 35) PL Poland - 36) PT Portugal\n37) RO Romania - 38) RS Serbia - 39) RU Russia\n40) SE Sweden - 41) SG Singapore - 42) SK Slovakia\n43) TR Turkey - 44) TW Taiwan - 45) UA Ukraine\n46) US United-States - 47) UZ Uzbekistan - 48) VN Viet-Nam\n49) ZA South-Africa")
 	
 	## Set colors is user selects nocolor option disable color
-	if (<<<$* grep "\-\-nocolor\|\-n" &> /dev/null); then
+	if (<<<$* grep -w "\-\-nocolor\|\-n" &> /dev/null); then
 		Green=$'\e[0m';
 		Yellow=$'\e[0m';
 		Red=$'\e[0m';
@@ -53,30 +57,44 @@ get_opts() {
 		ColorOff=$'\e[0m';
 	fi
 
-	if [ "$1" == "--nocolor" ] || [ "$1" == "-n" ]; then
-		shift
+	if (<<<$* grep -w "\-\-noconfirm\|\-q" &> /dev/null); then
+		confirm=false
 	fi
 
-	case "$1" in
-		-l|--list) list
-		;;
-		-h|--help) usage
-		;;
-		-c|--country)
-			if (<<<$countries grep -w "$2" &> /dev/null); then
-				country_code="$2"
-				get_list
-			else
-				echo
-				echo "${Red}Error: ${Yellow}country code: $2 not found."
-				echo "To view a list of country codes run:${Green} fetchmirrors -l${ColorOff}"
-				echo
-			fi
-		;;
-		*)
-			search
-		;;
-	esac
+	while (true)
+	  do
+		case "$1" in
+			-n|--nocolor) shift
+			;;
+			-q|--noconfirm) shift
+			;;
+			-l|--list) 
+				list
+				break
+			;;
+			-h|--help)
+				usage
+				break
+			;;
+			-c|--country)
+				if (<<<$countries grep -w "$2" &> /dev/null); then
+					country_code="$2"
+					get_list
+				else
+					echo
+					echo "${Red}Error: ${Yellow}country code: $2 not found."
+					echo "To view a list of country codes run:${Green} fetchmirrors -l${ColorOff}"
+					echo
+				fi
+				break
+			;;
+			*)
+				search
+				break
+			;;
+		esac
+	done
+	exit
 
 }
 
@@ -95,14 +113,19 @@ search() {
 			echo "${Red}Error: ${Yellow}please select a number from the list.${ColorOff}"
 		else
 			country_code=$(echo "$countries" | grep -o "$input...." | awk '{print $2}')
-			echo
-			echo -n "${Yellow}You have selected the country code:${Green} $country_code ${Yellow}- is this correct ${Green}[y/n]:${ColorOff} "
-			read input
-			case "$input" in
-				y|Y|yes|Yes|yY|Yy|yy|YY)
-					break
-				;;
-			esac
+			
+			if "$confirm" ; then
+				echo
+				echo -n "${Yellow}You have selected the country code:${Green} $country_code ${Yellow}- is this correct ${Green}[y/n]:${ColorOff} "
+				read input
+				case "$input" in
+					y|Y|yes|Yes|yY|Yy|yy|YY)
+						break
+					;;
+				esac
+			else
+				break
+			fi
 		fi
 	done
 
@@ -127,21 +150,25 @@ get_list() {
 	echo "${Yellow}Please wait while ranking${Green} $country_code mirrors...${ColorOff}"
 	rankmirrors -n 6 /tmp/mirrorlist > /tmp/mirrorlist.rank
 
-	echo
-	echo -n "${Yellow}Would you like to view new mirrorlist? ${Green}[y/n]: ${ColorOff}"
-	read input
+	if "$confirm" ; then
+		echo
+		echo -n "${Yellow}Would you like to view new mirrorlist? ${Green}[y/n]: ${ColorOff}"
+		read input
 
-	case "$input" in
-		y|Y|yes|Yes|yY|Yy|yy|YY)
-			echo
-			cat /tmp/mirrorlist.rank
-		;;
-	esac
+		case "$input" in
+			y|Y|yes|Yes|yY|Yy|yy|YY)
+				echo
+				cat /tmp/mirrorlist.rank
+			;;
+		esac
 
-	echo
-	echo -n "${Yellow}Would you like to install the new mirrorlist backing up existing? ${Green}[y/n]:${ColorOff} "
-	read input
-
+		echo
+		echo -n "${Yellow}Would you like to install the new mirrorlist backing up existing? ${Green}[y/n]:${ColorOff} "
+		read input
+	else
+		input="y"
+	fi
+		
 	case "$input" in
 		y|Y|yes|Yes|yY|Yy|yy|YY)
 			sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
